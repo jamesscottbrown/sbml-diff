27f9c028fa6efe227a69dac46ad95a253d8948ee
James Scott-Brown <james@jamesscottbrown.com>
2016-06-10 12:21:39 +0100

    Determine activator/repressor arrow directions numerically, rather than symbolically
    Previously, arrow directions were determined by symbolically testing whether kineticLaws
    were monotonic functions. However, this approach fails for the Hill activator function,
    as both the numerator and denominator are increasing functions of the activator concentration.
    
    The new approach is to set all parameters to one, and compare the numerical value of the
    kineticLaw when the concentration of the species of interest is 0 or 1. This fails to identify
    non-monotonic effects, but these are less common.
    
    The 'correct' approach of symbolically checking that the derivative of the kineticLaw w.r.t
    the species of interest is always non-negative or non-positive is too complex for the limited
    benefit it would provide.

--- sbml_diff/effect_direction.py
+++ sbml_diff/effect_direction.py
@@ -1,4 +1,6 @@
 from bs4 import NavigableString
+from rate_laws import convert_rate_law
+import math # needed for check_sign_numerically()
 
 
 def collate_interactions(child_classifications):
@@ -21,7 +23,6 @@ def collate_interactions(child_classifications):
 
 
 def invert_classification(classification):
-
     if classification in ["mixed", "constant"]:
         return classification
 
@@ -32,7 +33,7 @@ def invert_classification(classification):
         return "monotonic_increasing"
 
 
-def classify_basic_interaction(operator, child_classifications):
+def classify_basic_interaction_symbolically(operator, child_classifications):
     # if any children are mixed, so is result
     if "mixed" in child_classifications:
         return "mixed"
@@ -65,19 +66,22 @@ def classify_basic_interaction(operator, child_classifications):
         return collate_interactions(child_classifications)
 
 
-def categorise_interaction(kinetic_law, species_id):
-    for math in kinetic_law.select_one("math"):
-        if isinstance(math, NavigableString):
-            continue
-        return categorise_interaction_inner(math, species_id)
+def categorise_interaction(kinetic_law, species_id, categorise_symbolically=False):
+
+    if categorise_symbolically:
+        for math in kinetic_law.select_one("math"):
+            if isinstance(math, NavigableString):
+                continue
+            return categorise_interaction_inner(math, species_id)
+    else:
+        for math in kinetic_law.select_one("math"):
+            if isinstance(math, NavigableString):
+                continue
+            return categorise_interaction_inner_numerically(math, species_id)
 
 
 def categorise_interaction_inner(expression, species_id):
     # We implicitly assume constants and powers are positive.
-
-    # Stuff we still need to handle:
-    # pi, infinity, exponential2
-    # piecewise functions
 
     if expression.name == "cn":
         return "constant"
@@ -107,4 +111,43 @@ def categorise_interaction_inner(expression, species_id):
         child_classifications.append(categorise_interaction_inner(arg, species_id))
 
     # combine these classifications based on identity of function
-    return classify_basic_interaction(operator, child_classifications)
+    return classify_basic_interaction_symbolically(operator, child_classifications)
+
+
+def categorise_interaction_inner_numerically(expression, species_id):
+    # identify all parameters and concentrations in the rate law
+    symbols = []
+    for ci in expression.findAll("ci"):
+        symbols.append(ci.text.strip())
+    symbols = set(symbols)
+
+    return check_sign_numerically(expression, symbols, species_id)
+
+
+def check_sign_numerically(expr, param_names, species_id):
+    # This function will fail to report that a kineticLaw is mixed (rather than monotonic) if:
+    # - the sign of its gradient depends on value of one of the parameters (eg x^a/x^b)
+    # - the sign of its gradient depends on the concentration of the corresponding species
+
+    expr = convert_rate_law(expr)
+
+    replacement = {'^': '**', 'exp': 'math.exp', 'ln': 'math.log', 'log': 'math.log10', 'ceiling': 'math.ceil',
+                   'floor': 'math.floor', 'factorial': 'math.factorial', 'delay': '', 'pi': 'math.pi',
+                   'exponentiale': 'math.e', 'infinity': 'float("Inf")'}
+
+    for old in replacement:
+        expr = expr.replace(old, replacement[old])
+
+    param_names.remove(species_id)
+
+    for param in param_names:
+        expr = expr.replace(param, '1.0')
+
+    rate_change = eval(expr.replace(species_id, '1')) - eval(expr.replace(species_id, '0'))
+
+    if rate_change > 0:
+        return "monotonic_increasing"
+    elif rate_change == 0:
+        return "constant"
+    else:
+        return "monotonic_decreasing"
