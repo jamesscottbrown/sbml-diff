jamesscottbrown/sbml-diff

commit 552240b79058a77760f56a7f41d73b50eed5bd74

Avoid many searches using find() by saving the compartment of each species, and the reaction with each id

sbml_diff/sbml_diff.py
@@ -36,6 +36,30 @@ class SBMLDiff:
 
         self.models = map(lambda x: BeautifulSoup(x, 'xml'), self.model_strings)
 
+        # Avoid need to search for reactions by id
+        self.reactions = []
+        for model in self.models:
+            mr = {}
+            reaction_list = model.select_one("listOfReactions")
+            if reaction_list:
+                for reaction in reaction_list.select("reaction"):
+                    reaction_id = reaction.attrs["id"]
+                    mr[reaction_id] = reaction
+            self.reactions.append(mr)
+
+        # avoid need to keep finding reactant compartments
+        self.species_compartment = []
+        for model in self.models:
+            tmp = {}
+            species_list = model.select_one("listOfSpecies")
+            if species_list:
+                for species in species_list.select("species"):
+                    id = species.attrs["id"]
+                    compartment = species.attrs["compartment"]
+                    tmp[id] = compartment
+            self.species_compartment.append(tmp)
+
+
         if self.cartoon:
             self.elided_list = []
             self.elided_reactions = []
@@ -252,7 +276,7 @@ class SBMLDiff:
 
         for rule_target in rule_status:
             model_set = list(rule_status[rule_target])
-            inputs, compartment, rate_law = get_rule_details(self.models[model_set[0]], rule_target,
+            inputs, compartment, rate_law = get_rule_details(self.models[model_set[0]], rule_target, self.species_compartment[model_num],
                                                              draw_modifier_params=self.show_params)
 
             if compartment not in rule_strings.keys():
@@ -287,7 +311,7 @@ class SBMLDiff:
         rate_laws = ""
 
         for model_num, model in enumerate(self.models):
-            modifiers, compartment, rate_law = get_rule_details(model, target_id)
+            modifiers, compartment, rate_law = get_rule_details(model, target_id, self.species_compartment[model_num])
 
             if not rate_law:
                 return ""
@@ -347,7 +371,9 @@ class SBMLDiff:
 
         for reaction_id in reaction_status:
             model_set = list(reaction_status[reaction_id])
-            reactant_list, product_list, compartment, rate_law, _, _ = get_reaction_details(self.models[model_set[0]], reaction_id)
+            model_num = model_set[0]
+            reaction = self.reactions[model_num][reaction_id]
+            reactant_list, product_list, compartment, rate_law, _, _ = get_reaction_details(self.models[model_num], reaction, self.species_compartment[model_num])
 
             if compartment not in reaction_strings.keys():
                 reaction_strings[compartment] = []
@@ -385,7 +411,8 @@ class SBMLDiff:
         ever_drawn = False
 
         for model_num, model in enumerate(self.models):
-            reactants, products, compartment, rate_law, rs, ps = get_reaction_details(model, reaction_id)
+            reaction = self.reactions[model_num][reaction_id]
+            reactants, products, compartment, rate_law, rs, ps = get_reaction_details(model, reaction, self.species_compartment[model_num])
 
             reaction = model.select_one("listOfReactions").find(id=reaction_id)
             if not reaction:
@@ -645,9 +672,9 @@ class SBMLDiff:
         arrow_status = {}
         for model_num, model in enumerate(self.models):
             if self.cartoon:
-                arrows = get_regulatory_arrow(model, compartment_id, elided_reactions=self.elided_reactions[model_num])
+                arrows = get_regulatory_arrow(model, compartment_id, self.reactions[model_num], self.species_compartment[model_num], elided_reactions=self.elided_reactions[model_num])
             else:
-                arrows = get_regulatory_arrow(model, compartment_id)
+                arrows = get_regulatory_arrow(model, compartment_id, self.reactions[model_num], self.species_compartment[model_num])
 
             for ind, arrow in enumerate(arrows):
                 if arrow not in arrow_status.keys():
@@ -699,7 +726,7 @@ class SBMLDiff:
             self.draw_modified_params()
         self.generate_dot.print_footer()
 
-    def abstract_model(self, model):
+    def abstract_model(self, model, model_num):
         """
         For each pair of species in a model, determine if there is an interaction, and if so classify it.
         If a species is a reactant, it is not considered to interact with itself through that reaction, since any species
@@ -708,6 +735,8 @@ class SBMLDiff:
         Parameters
         ----------
         model : bs4.BeautifulSoup object produced by parsing an SBML model
+
+        model_num : index of the model being abstracted
 
         Returns
         -------
@@ -731,8 +760,9 @@ class SBMLDiff:
                 interactions[modifier][target] = set()
 
         reactions = get_reactions(model)
-        for reaction in reactions:
-            reactant_list, product_list, compartment, rate_law, _, _ = get_reaction_details(model, reaction)
+        for reaction_id in reactions:
+            reaction = self.reactions[model_num][reaction_id]
+            reactant_list, product_list, compartment, rate_law, _, _ = get_reaction_details(model, reaction, self.species_compartment[model_num])
 
             # Identify all species that appear in kineticLaw
             modifiers = []
@@ -791,7 +821,7 @@ class SBMLDiff:
         models_containing_species = {}
 
         for model_num, model in enumerate(self.models):
-            abstract, species = self.abstract_model(model)
+            abstract, species = self.abstract_model(model, model_num)
 
             abstracted_model.append(abstract)
             species_list = species_list.union(species)

sbml_diff/accessor_functions.py
@@ -31,7 +31,7 @@ def get_params(model):
     return set(param_ids), param_values
 
 
-def get_regulatory_arrow(model, compartment, elided_reactions=False):
+def get_regulatory_arrow(model, compartment, reactions, species_compartments, elided_reactions=False):
     """
     Find all regulatory interactions in a particular compartment of a model, and construct an array of strings
     representing these.
@@ -58,14 +58,14 @@ def get_regulatory_arrow(model, compartment, elided_reactions=False):
 
     arrows = []
 
-    reaction_list = model.select_one("listOfReactions")
-    if not reaction_list:
+    if not reactions:
         return arrows
 
-    for reaction in reaction_list.select("reaction"):
+    for reaction_id in reactions:
+        reaction = reactions[reaction_id]
         if reaction in elided_reactions:
             continue
-        reaction_id = reaction.attrs["id"]
+
         for ci in reaction.select_one("kineticLaw").select("ci"):
 
             # Check if this is a species id (it could validly be a species/compartment/parameter/function/reaction id)
@@ -74,7 +74,8 @@ def get_regulatory_arrow(model, compartment, elided_reactions=False):
                 continue
 
             # if not a reactant, add regulatory arrow
-            reactant_list, product_list, compartment, rate_law, _, _ = get_reaction_details(model, reaction_id)
+
+            reactant_list, product_list, compartment, rate_law, _, _ = get_reaction_details(model, reaction, species_compartments)
             if species_id in reactant_list:
                 continue
 
@@ -109,7 +110,7 @@ def get_species(model, compartment_id):
     return ids
 
 
-def get_species_compartment(model, species_id):
+def get_species_compartment(model, species_id, species_compartments):
     """
     Get the id of the compartment containing a species.
     Report params as belonging to compartment 'NONE'
@@ -126,20 +127,17 @@ def get_species_compartment(model, species_id):
     compartment : id of the compartment
 
     """
-    species_list = model.select_one("listOfSpecies")
 
-    if not species_list:
+    if not species_compartments:
         return "NONE"
 
-    species = species_list.find(id=species_id)
-
-    if not species:
+    if species_id in species_compartments.keys():
+        return  species_compartments[species_id]
+    else:
         return "NONE"
 
-    return species.attrs["compartment"]
 
-
-def get_reaction_details(model, reaction_id):
+def get_reaction_details(model, reaction, species_compartments):
     """
     Get details of a single reaction.
 
@@ -148,9 +146,9 @@ def get_reaction_details(model, reaction_id):
     Parameters
     ----------
     model : bs4.BeautifulSoup object produced by parsing an SBML model
-        
-    reaction_id : id of the reaction of interest
-        
+
+    reaction : bs4.BeautifulSoup object of the reaction of interest
+
 
     Returns
     -------
@@ -167,7 +165,6 @@ def get_reaction_details(model, reaction_id):
     product_stoichiometries : list of stoichiometries of products
 
     """
-    reaction = model.select_one("listOfReactions").find(id=reaction_id)
 
     if not reaction:
         return [], [], False, False, [], []
@@ -190,8 +187,8 @@ def get_reaction_details(model, reaction_id):
             reactant_list.append(species)
 
             if not compartment:
-                compartment = get_species_compartment(model, species)
-            if compartment != get_species_compartment(model, species):
+                compartment = get_species_compartment(model, species, species_compartments)
+            if compartment != get_species_compartment(model, species, species_compartments):
                 compartment = "NONE"
 
     products = reaction.select_one("listOfProducts")
@@ -212,8 +209,8 @@ def get_reaction_details(model, reaction_id):
 
             # if reaction has no reactants, try to categorise by products instead
             if not compartment:
-                compartment = get_species_compartment(model, species)
-            if compartment != get_species_compartment(model, species):
+                compartment = get_species_compartment(model, species, species_compartments)
+            if compartment != get_species_compartment(model, species, species_compartments):
                 compartment = "NONE"
 
     rate_law = reaction.select_one("kineticLaw").select_one("math")
@@ -245,7 +242,7 @@ def get_reactions(model):
     return reactions
 
 
-def get_rule_details(model, target_id, draw_modifier_params=True):
+def get_rule_details(model, target_id, species_compartments, draw_modifier_params=True):
     """
     Given the id of a species affected by a rule, find details of that rule.
 
@@ -291,7 +288,7 @@ def get_rule_details(model, target_id, draw_modifier_params=True):
 
         modifiers.append(species_id)
 
-    compartment = get_species_compartment(model, target).strip()
+    compartment = get_species_compartment(model, target, species_compartments).strip()
     rate_law = rule.select_one("math")
     return modifiers, compartment, rate_law
 

commit f4b222cbc7f6b5354338dcee98bd7a500f92906a

Fix bug introduced by 552240b

sbml_diff/sbml_diff.py
@@ -430,12 +430,11 @@ class SBMLDiff:
         ever_drawn = False
 
         for model_num, model in enumerate(self.models):
-            reaction = self.reactions[model_num][reaction_id]
-            reactants, products, compartment, rate_law, rs, ps = get_reaction_details(model, reaction, self.species_compartment[model_num])
-
-            reaction = model.select_one("listOfReactions").find(id=reaction_id)
-            if not reaction:
+            if reaction_id not in self.reactions[model_num].keys():
                 continue
+            reaction = self.reactions[model_num][reaction_id]
+
+            reactants, products, compartment, rate_law, rs, ps = get_reaction_details(model, reaction, self.species_compartment[model_num])
 
             # Skip processing reaction if it should not be drawn for this model
             show_reaction = True